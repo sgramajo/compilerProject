41856294f9b75d71d292e8df3181a1c1a1acbbad
Alexei Gramajo <Alexei Gramajo>
2017-02-11 13:05:38 -0500

    Virtual Machine Complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,16 +20,45 @@ window.onload = function() {
         }
         reader.readAsText(file);	
     });
+
+    /* Add Text */
+    $("#addText").keydown(function(evt) {
+		if(evt.keyCode == 13){
+            alert($("#addText").val());
+        }
+    }); 
+    
+    /*Button Group */
+    $("div.btn-group").on( 'click', '.dropdown-menu li', function( event ) {
+      var $target = $( event.currentTarget );
+      $target.closest( '.btn-group' )
+         .find( '[data-bind="label"]' ).text( $target.text() )
+            .end()
+         .children( '.dropdown-toggle' ).dropdown( 'toggle' );
+
+      return false;
+
+   });
+   restartConsole(); 
 }
 
+function restartConsole(){
+    /* Console Startup */
+    $(consoleResults).empty(); 
+    $(consoleResults).append("<span>\t>>\t</span>"); 
+}
 /* New File */
 function newFile(){
     editor.setValue("");
     editor.clearHistory(); 
+    restartConsole(); 
 }
 function goback(){
     editor.undo();
 }
 function redo(){
     editor.redo(); 
+}
+function trimLeadZero(s) {
+    return (""+s).replace(/^0+/, "");
 }
--- app/scripts/virtualMachine.js
+++ app/scripts/virtualMachine.js
@@ -17,33 +17,42 @@ var previous_instruction = 0;
 var nIR = new Object(); //needs int op, int l, int m
 var halt = true;
 var idResults = "#results"; //where the results will be placed
+var consoleResults = "#console"; //where the console results will be placed 
 
+//initializing arrays
+var code = []; 
+//Initial "stack" store values -- All must be 0's
+var stack = Array.apply(null, Array(MAX_STACK_HEIGHT)).map(Number.prototype.valueOf,0);
+var bar = Array.apply(null, Array(MAX_STACK_HEIGHT)).map(Number.prototype.valueOf,0);
+
+function addToConsole(text){ //with Date
+    $('#console span').last().remove();
+    $(consoleResults).append("\t>>\t[" + (new Date($.now())).getHours() + ":" + (new Date($.now())).getMinutes() + ":" + (new Date($.now())).getSeconds() + "] " + text); 
+    $(consoleResults).append("\t>>\t<span></span>"); 
+}
+function addConsoleText(text){ //Only Text
+    $(consoleResults).append("[Reminder] " + text); 
+    $(consoleResults).append("\t>>\t<span></span>"); 
+}
 function main(){
+    addToConsole("Starting PM/O Machine ...\n"); 
     // Initial values for the PM/O CPU registers
     var i=0;
     var j=0;
     nIR = {op: 0, l: 0, m: 0}; 
-    alert("Here"); 
-    /*initializing the top part of the file
+    //initializing the top part of the file
     $(idResults).append("Line\t OP\t L\t M\n");
     //Saving the instructions onto a "code" store location, an array
-    while(!feof(ifp)){
-        instruction instruc;
-        fscanf(ifp,"%d%d%d", &(instruc.op), &(instruc.l), &(instruc.m));
-        code[i].op = instruc.op;
-        code[i].l = instruc.l;
-        code[i].m = instruc.m;
+    while(i < editor.lineCount()){
+        var instruction = editor.getLine(i).split(" "); 
+        var oneInstruc = {op: instruction[0], l: instruction[1], m: instruction[2]}; 
+        code.push(oneInstruc); 
         print_function(i, code[i].op, code[i].l, code[i].m);
         i++;
     }
-
-    //Initial "stack" store values -- All must be 0's
-    for(j=0; j< MAX_STACK_HEIGHT; j++){
-        stack[j] = 0;
-        bar[j] = 0;
-    }
-    $(idResults).append("\t\t\t\t\t PC\t BP\t SP\t stack\n");
-    $(idResults).append("Initial values\t\t " + nPC + "\t " + nBP + "\t " + nSP + "\n");
+    
+    $(idResults).append("\t\t\t\t PC\t BP\t SP\t stack\n");
+    $(idResults).append("Initial values\t\t\t " + nPC + "\t " + nBP + "\t " + nSP + "\n");
 
     i=0;
     //execute the instructions - PM/O instruction cycle carries out two steps, fetch and execute
@@ -51,7 +60,8 @@ function main(){
         fetch();
         execute();
         previous_instruction = nPC;
-    }*/
+   }
+   
 }//end of main function
 
 /*
@@ -62,49 +72,50 @@ function main(){
                 nM = A Number, A Program Address, or the identity of the operator
 */
 function print_function(temp, nOP, nL, nM){
+    
     switch(nOP){
-    case 01:
+    case "1":
         $(idResults).append(temp + "\t LIT\t " + nL + "\t " + nM + "\n");
         break;
 
-    case 2:
+    case "2":
         $(idResults).append(temp + "\t OPR\t " + nL + "\t " + nM + "\n");
         break;
 
-    case 3:
+    case "3":
         $(idResults).append(temp + "\t LOD\t " + nL + "\t " + nM + "\n");
         break;
 
-    case 4:
+    case "4":
         $(idResults).append(temp + "\t STO\t " + nL + "\t " + nM + "\n");
         break;
 
-    case 5:
+    case "5":
         $(idResults).append(temp + "\t CAL\t " + nL + "\t " + nM + "\n");
         break;
 
-    case 6:
+    case "6":
         $(idResults).append(temp + "\t INC\t " + nL + "\t " + nM + "\n");
         break;
 
-    case 7:
+    case "7":
         $(idResults).append(temp + "\t JMP\t " + nL + "\t " + nM + "\n");
         break;
 
-    case 8:
+    case "8":
         $(idResults).append(temp + "\t JPC\t " + nL + "\t " + nM + "\n");
         break;
 
-    case 9:
+    case "9":
         $(idResults).append(temp + "\t SIO\t" + nL + "\t " + nM + "\n");
         break;
 
-    case 10:
-        $(idResults).append(ofp, "%d\t SIO\t " + nL + "\t " + nM + "\n");
+    case "10":
+        $(idResults).append(temp + "\t SIO\t " + nL + "\t " + nM + "\n");
         break;
 
-    case 11:
-        $(idResults).append(ofp, "%d\t SIO\t " + nL + "\t " + nM + "\n");
+    case "11":
+        $(idResults).append(temp + "\t SIO\t " + nL + "\t " + nM + "\n");
         break;
     }
 
@@ -140,85 +151,89 @@ function fetch(){
     M is the operator + runs the correct arithmetic/logical instruction
 */
 function execute(){
+    nSP = trimLeadZero(nSP); 
     switch(nIR.op){ //Explanations are taken for the instructions are taken from the HW instructions sheet - A copy is in the same folder where the code is being held
-        case 1: //LIT O,M --- Pushes constant value (literal) M onto the stack
-            nSP= nSP + 1;
+        case "1": //LIT O,M --- Pushes constant value (literal) M onto the stack
+            nSP= Number(nSP) + 1;
             stack[nSP] = nIR.m;
             $(idResults).append(previous_instruction + "\t LIT\t " + nIR.l + "\t " + nIR.m + "\t " + nPC + "\t " + nBP + "\t " + nSP + "\t");
             print_operation();
             break;
 
-        case 2: //OPR 0, M --- Operation to be performed on the data at the top of the stack
+        case "2": //OPR 0, M --- Operation to be performed on the data at the top of the stack
             logical_operation();
             $(idResults).append(previous_instruction + "\t OPR\t " + nIR.l + "\t " + nIR.m + "\t " + nPC + "\t " + nBP + "\t " + nSP + "\t");
             print_operation();
             break;
 
-        case 3://LOD L,M -- Load value into top of stack from the stack location at offset M
+        case "3"://LOD L,M -- Load value into top of stack from the stack location at offset M
                 //from L lexicographical levels down
-            nSP = nSP + 1;
-            stack[nSP] = stack[base(nIR.l, nBP) + nIR.m];
+            nSP = Number(nSP) + 1;
+            stack[nSP] = stack[base_function(nIR.l, nBP) + Number(nIR.m)];
             $(idResults).append(previous_instruction + "\t LOD\t " + nIR.l + "\t " + nIR.m + "\t " + nPC + "\t " + nBP + "\t " + nSP + "\t");
             print_operation();
             break;
 
-        case 4://STO L, M --- Store value at top of stack in the stack location at offset M
+        case "4"://STO L, M --- Store value at top of stack in the stack location at offset M
                 //from L lexicographical levels down
-            stack[base(nIR.l, nBP) + nIR.m] = stack[nSP];
-            nSP = nSP - 1;
+            stack[base_function(nIR.l, nBP) + Number(nIR.m)] = stack[nSP];
+            nSP = Number(nSP) - 1;
             $(idResults).append(previous_instruction + "\t STO\t " + nIR.l + "\t " + nIR.m + "\t " + nPC + "\t " + nBP + "\t " + nSP + "\t");
             print_operation();
             break;
 
-        case 5://Call procedure at code index M (generates new Activation REcord and Pc<- M)
-            stack[nSP + 1] = 0;                    //Space to return value (FV)
-            stack[nSP + 2] = base(nIR.l, nBP);     //static link(SL)
-            stack[nSP + 3] = nBP;                  //dynamic link (DL)
-            stack[nSP + 4] = nPC;                   //Return Address (RA)
-            nBP = nSP +1;
+        case "5"://Call procedure at code index M (generates new Activation REcord and Pc<- M)
+            stack[Number(nSP) + 1] = 0;                    //Space to return value (FV)
+            stack[Number(nSP) + 2] = base_function(nIR.l, nBP);     //static link(SL)
+            stack[Number(nSP) + 3] = nBP;                  //dynamic link (DL)
+            stack[Number(nSP) + 4] = nPC;                   //Return Address (RA)
+            nBP = Number(nSP) + 1;
             nPC = nIR.m;
             $(idResults).append(previous_instruction + "\t CAL\t " + nIR.l + "\t " + nIR.m + "\t " + nPC + "\t " + nBP + "\t " + nSP + "\t");
             print_operation();
             bar[nSP] = 1;
             break;
 
-        case 6://INC 0,M --- Allocate M locas (increment sp by M). First four are Functional Value
+        case "6"://INC 0,M --- Allocate M locas (increment sp by M). First four are Functional Value
                 //(FV, also calle dreturn value), Static Links(SL), Dynamic Link (DL), and Return Address (RA)
-            nSP = nSP + nIR.m;
+            nSP = Number(nSP) + Number(nIR.m);
             $(idResults).append(previous_instruction + "\t INC\t " + nIR.l + "\t " + nIR.m + "\t " + nPC + "\t " + nBP + "\t " + nSP + "\t");
             print_operation();
             break;
 
-        case 7: //JMP 0, M --- Jump to instruction M
+        case "7": //JMP 0, M --- Jump to instruction M
             nPC = nIR.m;
             $(idResults).append(previous_instruction + "\t JMP\t " + nIR.l + "\t " + nIR.m + "\t " + nPC + "\t " + nBP + "\t " + nSP + "\t");
             print_operation();
             break;
 
-        case 8:// JPC 0, M --- Jump to instruction M if top stack element is 0.
+        case "8":// JPC 0, M --- Jump to instruction M if top stack element is 0.
             if(stack[nSP] == 0){
                 nPC = nIR.m;
             }
-            nSP = nSP - 1;
+            nSP = Number(nSP) - 1;
             $(idResults).append(previous_instruction + "\t JPC\t " + nIR.l + "\t " + nIR.m + "\t " + nPC + "\t " + nBP + "\t " + nSP + "\t");
             print_operation();
             break;
 
-        case 9: //SIO 0,1 --- Write the top stack element to the screen
-            $(idResults).append(stack[nSP]);
-            nSP = nSP - 1;
+        case "9": //SIO 0,1 --- Write the top stack element to the screen
+            $(consoleResults).append("Element on the top of the stack\n"); 
+            $(consoleResults).append("\t>>\t" + stack[nSP]);
+            nSP = Number(nSP) - 1;
             $(idResults).append(previous_instruction + "\t SIO\t " + nIR.l + "\t " + nIR.m + "\t " + nPC + "\t " + nBP + "\t " + nSP + "\t");
             print_operation();
             break;
 
-        case 10: //SIO 0, 2 --- Read in input from the user and store it at the top of the stack
-            nSP= nSP + 1;
-            //scanf("%d", &stack[nSP]);
-           $(idResults).append(previous_instruction + "\t SIO\t " + nIR.l + "\t " + nIR.m + "\t " + nPC + "\t " + nBP + "\t " + nSP + "\t");
+        case "10": //SIO 0, 2 --- Read in input from the user and store it at the top of the stack
+            nSP= Number(nSP) + 1;
+            addConsoleText("Type number to put in top of the stack ...\n"); 
+            stack[nSP] = 4; //change for later
+            //$(consoleResults).append("<input type='text' autofocus id='addText'>"); 
+            $(idResults).append(previous_instruction + "\t SIO\t " + nIR.l + "\t " + nIR.m + "\t " + nPC + "\t " + nBP + "\t " + nSP + "\t");
             print_operation();
             break;
 
-        case 11: //SIO 0, 3 --- Halt the machine
+        case "11": //SIO 0, 3 --- Halt the machine
             halt = false;
             $(idResults).append(previous_instruction + "\t SIO\t " + nIR.l + "\t " + nIR.m + "\t " + nPC + "\t " + nBP + "\t " + nSP + "\t");
             print_operation();
@@ -230,8 +245,9 @@ function execute(){
 /* print_operation prints out the stack into the file */
 function print_operation(){
     var k;
-    for(k=1; k < (nSP + 1); k++){
-        $(idResults).append(stack[k]);
+    var temp = Number(nSP) + 1; 
+    for(k = 1; k < temp; k++){
+        $(idResults).append(stack[k] + " ");
         if(bar[k] == 1){
             $(idResults).append("| ");
         }
@@ -245,74 +261,74 @@ function print_operation(){
 
 function logical_operation(){
     switch(nIR.m){
-    case 00://RETURN
+    case "0"://RETURN
         nSP= nBP -1;
         nPC = stack[nSP + 4];
         nBP = stack[nSP + 3];
         bar[nSP]= 0;
         break;
 
-    case 01://NEG
+    case "1"://NEG
         stack[nSP] = stack[nSP] * -1;
         break;
 
-    case 02://ADD
-        nSP = nSP -1;
-        stack[nSP] = stack[nSP] + stack[nSP + 1];
+    case "2"://ADD
+        nSP = Number(nSP) - 1;
+        stack[nSP] = stack[nSP] + stack[Number(nSP) + 1];
         break;
 
-    case 03://SUB
-        nSP = nSP -1;
-        stack[nSP] = stack[nSP] - stack[nSP + 1];
+    case "3"://SUB
+        nSP = Number(nSP) -1;
+        stack[nSP] = stack[nSP] - stack[Number(nSP) + 1];
         break;
 
-    case 04://MUL
-        nSP= nSP -1;
-        stack[nSP]= stack[nSP] * stack[nSP +1];
+    case "4"://MUL
+        nSP= Number(nSP) -1;
+        stack[nSP]= stack[nSP] * stack[Number(nSP) +1];
         break;
 
-    case 05://DIV
-        nSP= nSP -1;
-        stack[nSP] = stack[nSP] / stack[nSP +1];
+    case "5"://DIV
+        nSP= Number(nSP) -1;
+        stack[nSP] = stack[nSP] / stack[Number(nSP) +1];
         break;
 
-    case 6://ODD
+    case "6"://ODD
         stack[nSP] = stack[nSP] % 2;
         break;
 
-    case 7://MOD
-        nSP= nSP -1;
-        stack[nSP]= (stack[nSP]) % (stack[nSP + 1]);
+    case "7"://MOD
+        nSP= Number(nSP) -1;
+        stack[nSP]= (stack[nSP]) % (stack[Number(nSP) + 1]);
         break;
 
-    case 8://EQL
-        nSP= nSP -1;
-        stack[nSP]= stack[nSP] == stack[nSP + 1];
+    case "8"://EQL
+        nSP= Number(nSP) -1;
+        stack[nSP]= (stack[nSP] == stack[Number(nSP) + 1]) ? 1 : 0;
         break;
 
-    case 9://NEQ
-        nSP= nSP -1;
-        stack[nSP]= stack[nSP] != stack[nSP + 1];
+    case "9"://NEQ
+        nSP= Number(nSP) -1;
+        stack[nSP]= (stack[nSP] != stack[Number(nSP) + 1]) ? 1 : 0;
         break;
 
-    case 10://LSS
-        nSP= nSP -1;
-        stack[nSP]= stack[nSP] < stack[nSP + 1];
+    case "10"://LSS
+        nSP= Number(nSP) -1;
+        stack[nSP]= (stack[nSP] < stack[Number(nSP) + 1]) ? 1 : 0;
         break;
 
-    case 11://LEQ
-        nSP= nSP -1;
-        stack[nSP]= stack[nSP] <= stack[nSP + 1];
+    case "11"://LEQ
+        nSP= Number(nSP) -1;
+        stack[nSP]= (stack[nSP] <= stack[Number(nSP) + 1]) ? 1 : 0;
         break;
 
-    case 12://GTR
-        nSP= nSP -1;
-        stack[nSP]= stack[nSP] > stack[nSP + 1];
+    case "12"://GTR
+        nSP= Number(nSP) -1;
+        stack[nSP]= (stack[nSP] > stack[Number(nSP) + 1]) ? 1 : 0;
         break;
 
-    case 13://GEO
-        nSP= nSP -1;
-        stack[nSP]= stack[nSP] >= stack[nSP + 1];
+    case "13"://GEO
+        nSP= Number(nSP) -1;
+        stack[nSP]= (stack[nSP] >= stack[Number(nSP) + 1]) ? 1 : 0;
         break;
 
     }//end of switch statement
